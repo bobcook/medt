b565730a774ce1fb8f0b02fa0d6174754142c8a9
Bob Cook <bobcook@sunrem.com>
2015-01-09 14:16:58 -0700

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
+
+  comfy_route :cms_admin, :path => '/admin'
+
+  # Make sure this routeset is defined last
+  comfy_route :cms, :path => '/', :sitemap => false
+
+
 end
